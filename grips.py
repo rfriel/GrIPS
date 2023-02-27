e73a1542269aaa47d7efe985e8ef5e35e6258094
Robert Friel <rob@rungalileo.io>
2023-02-27 16:38:08 -0500

    work

--- grips.py
+++ grips.py
@@ -14,6 +14,8 @@ import json
 import torch
 nltk.download('punkt')
 
+parser = Parser.load('crf-con-en')
+
 
 para_model_name = 'tuner007/pegasus_paraphrase'
 torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
